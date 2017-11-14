02d5cfe152436be6ee671f0230256b25d4e3adad
Roman Meerson <homich1991@gmail.com>
2017-11-14 16:37:11 +0400

    feign

--- developer/src/main/java/com/epam/springclouddemo/developer/DeepThoughtCaller.java
+++ developer/src/main/java/com/epam/springclouddemo/developer/DeepThoughtCaller.java
@@ -2,26 +2,17 @@ package com.epam.springclouddemo.developer;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DeepThoughtCaller {
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @Autowired
-    private RestTemplate restTemplate;
+    private DeepThoughtClient deepThoughtClient;
 
     @HystrixCommand(fallbackMethod = "fallback")
     public Integer getStoryPoints(int hours) {
-        return restTemplate.getForObject("http://deep-thought/convert/" + hours,
-                Integer.class);
+        return deepThoughtClient.convert(hours);
     }
 
     private Integer fallback(int hours) {
--- developer/src/main/java/com/epam/springclouddemo/developer/DeveloperApplication.java
+++ developer/src/main/java/com/epam/springclouddemo/developer/DeveloperApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.Random;
 @RestController
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableFeignClients(clients = DeepThoughtClient.class)
 public class DeveloperApplication {
 
     @Autowired
