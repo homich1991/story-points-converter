5337114ec1343713e7cabc8a36ae233d19158819
Roman Meerson <homich1991@gmail.com>
2018-04-03 21:42:37 +0400

    merge fix

--- developer/src/main/java/com/epam/springclouddemo/developer/DeepThoughtClient.java
+++
@@ -1,8 +0,0 @@
-package com.epam.springclouddemo.developer;
-
-import com.epam.springclouddemo.deepthought.DeepThought;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient("deep-thought")
-public interface DeepThoughtClient extends DeepThought{
-}
