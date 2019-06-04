2be8718fe1e397a23a6bdf45c1f809f69a56bfba
Roman Meerson <homich1991@gmail.com>
2019-06-04 22:03:36 +0200

    upgrade feign to openfeign

--- developer/src/main/java/com/epam/developer/DeveloperApplication.java
+++ developer/src/main/java/com/epam/developer/DeveloperApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
