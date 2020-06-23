1fd718447fd5c47b8d5f9ded1f256c3d741cf1e3
Roman Meerson <homich1991@gmail.com>
2020-06-23 16:09:33 +0200

    Update DeepThoughtApplication.java

--- deep-thought/src/main/java/com/epam/deepthought/DeepThoughtApplication.java
+++ deep-thought/src/main/java/com/epam/deepthought/DeepThoughtApplication.java
@@ -2,7 +2,6 @@ package com.epam.deepthought;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
